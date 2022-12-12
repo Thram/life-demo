40e229d15d8a9273e527449d15ec932571095b99
Thram <thramposo@gmail.com>
2022-12-13 02:22:07 +1300

    Cleanup branch

--- src/components/Button.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import { createEventDispatcher } from "svelte";
-  const dispatch = createEventDispatcher();
-  let label = "Button";
-  let color = "red";
-</script>
-
-<button on:click={() => dispatch("click")}>{label}</button>
--- src/stores/rules.ts
+++
@@ -1,9 +0,0 @@
-import { writable } from 'svelte/store';
-
-
-
-export const store = writable(0);
-
-export const update = () => {
-    store.update(n => n + 1);
-}
